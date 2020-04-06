984ca921a5a1192a74ad05aa06b6020b088909da
lanfrye <louis.anfrye@sts-sio-caen.info>
2020-04-06 17:16:15 +0200

    connection

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+basse de donée de discord
+{
+privée $ _nomBase ;
+privée $ _utilisateur;
+privée $ _mdp;
+privée $ _nomServeur;	
+
+connection aux fonctions publiques select ( $ nomGroupe)
+}
+?>
